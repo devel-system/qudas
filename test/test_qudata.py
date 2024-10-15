ca6d7ef7081149edff961235eb7a266da263b509
keichiroHiga <higa.devel@gmail.com>
2024-10-15 21:46:38 +0900

    update: outputを追加。

--- test/test_qudata.py
+++ test/test_qudata.py
@@ -2,7 +2,7 @@ import unittest
 from qudas.qudata import QuData
 
 # その他必要なパッケージ
-from amplify import VariableGenerator, Poly
+from amplify import VariableGenerator, Model, Poly, FixstarsClient, solve
 from pyqubo import Binary, Base
 from pyqubo.utils.asserts import assert_qubo_equal
 import numpy as np
@@ -10,8 +10,11 @@ import pandas as pd
 import pulp
 import networkx as nx
 import dimod
-from sympy import Symbol
+from sympy import Symbol, symbols, lambdify
+from scipy.optimize import minimize, Bounds
+from datetime import timedelta
 import os
+
 
 def dicts_are_equal(dict1, dict2):
     """辞書のキーの順序を無視して等価性を比較する関数"""
@@ -30,6 +33,9 @@ def dicts_are_equal(dict1, dict2):
 
 class TestQudata(unittest.TestCase):
 
+    ##############################################
+    # QuDataInput
+    ##############################################
     def test_init_with_dict(self):
         """辞書データで初期化する場合のテスト"""
         prob = {('q0', 'q1'): 1.0, ('q2', 'q2'): -1.0}
@@ -385,6 +391,154 @@ class TestQudata(unittest.TestCase):
         prob_sympy = q0_sympy * q1_sympy - q2_sympy
 
         self.assertEqual(expr, prob_sympy)
+
+    ##############################################
+    # QuDataOutput
+    ##############################################
+    def test_from_pulp(self):
+        # PuLP問題を解く
+        prob = pulp.LpProblem("Test Problem", pulp.LpMinimize)
+        x = pulp.LpVariable('x', lowBound=0, upBound=1, cat='Binary')
+        y = pulp.LpVariable('y', lowBound=0, upBound=1, cat='Binary')
+        prob += 2*x - y
+        prob.solve()
+
+        # QuDataOutputのインスタンスを生成し、from_pulpメソッドをテスト
+        qdo = QuData.output().from_pulp(prob)
+
+        # 期待される結果
+        expected_result = {
+            'variables': {'x': 0, 'y': 1},
+            'objective': -1
+        }
+
+        # 検証
+        self.assertEqual(qdo.result, expected_result)
+        self.assertEqual(qdo.result_type, 'pulp')
+
+    def test_from_amplify(self):
+        # Amplify形式の結果
+        gen = VariableGenerator()
+        q = gen.array("Binary", shape=(3))
+        objective = 2 * q[0] - q[1] - q[2]
+
+        # ソルバーの設定
+        client = FixstarsClient()
+        client.token = "AE/HaqGh1iuFMEennXk10xS1LCgld8D18oC"
+        client.parameters.timeout = timedelta(milliseconds=100)
+
+        # 最小化を実行
+        amplify_result = solve(Model(objective), client)
+
+        # QuDataOutputのインスタンスを生成し、from_amplifyメソッドをテスト
+        qdo = QuData.output().from_amplify(amplify_result)
+
+        # 期待される結果
+        expected_result = {
+            'variables': {'q_0': 0.0, 'q_1': 1.0, 'q_2': 1.0},
+            'objective': -2
+        }
+
+        # 検証
+        self.assertEqual(qdo.result, expected_result)
+        self.assertEqual(qdo.result_type, 'amplify')
+
+    def test_from_dimod(self):
+        # Dimod形式の結果
+        qubo = {('q0', 'q0'): 2, ('q1', 'q1'): -1, ('q2', 'q2'): -1}
+        sampleset = dimod.ExactSolver().sample_qubo(qubo)
+
+        # QuDataOutputのインスタンスを生成し、from_dimodメソッドをテスト
+        qdo = QuData.output().from_dimod(sampleset)
+
+        # 期待される結果
+        expected_result = {
+            'variables': {'q0': 0.0, 'q1': 1.0, 'q2': 1.0},
+            'objective': -2
+        }
+
+        # 検証
+        self.assertEqual(qdo.result, expected_result)
+        self.assertEqual(qdo.result_type, 'dimod')
+
+    def test_from_scipy(self):
+
+        # シンボリック変数の定義
+        q0, q1, q2 = symbols('q0 q1 q2')
+
+        # 目的関数を定義
+        objective_function = 2 * q0 - q1 - q2
+
+        # シンボリック関数を数値化して評価できる形式に変換
+        f = lambdify([q0, q1, q2], objective_function, 'numpy')
+
+        # 初期解 (すべて0に設定)
+        q = [0.5, 0.5, 0.5]
+
+        # バイナリ変数の範囲を定義 (0 <= x <= 1)
+        bounds = Bounds([0, 0, 0], [1, 1, 1])
+
+        # 制約なしで最適化を行い、結果をバイナリ制約に丸める
+        res = minimize(lambda q: f(q[0], q[1], q[2]), q, method='SLSQP', bounds=bounds)
+
+        # QuDataOutputのインスタンスを生成し、from_scipyメソッドをテスト
+        qdo = QuData.output().from_scipy(res)
+
+        # 期待される結果
+        expected_result = {
+            'variables': {'q0': 0.0, 'q1': 1.0, 'q2': 1.0},
+            'objective': -2
+        }
+
+        # 検証
+        self.assertEqual(qdo.result, expected_result)
+        self.assertEqual(qdo.result_type, 'sympy')
+
+    def test_to_dimod(self):
+        # Dimod形式の結果に変換
+        qdo = QuData.output(result={
+            'variables': {'q0': 0.0, 'q1': 1.0, 'q2': 1.0},
+            'objective': -2
+        })
+        dimod_result = qdo.to_dimod()
+
+        # Dimod形式の結果
+        qubo = {('q0', 'q0'): 2, ('q1', 'q1'): -1, ('q2', 'q2'): -1}
+        sampleset = dimod.ExactSolver().sample_qubo(qubo)
+
+        # 検証
+        self.assertEqual(dimod_result.first, sampleset.first)
+
+    def test_to_scipy(self):
+        # SciPy形式の結果に変換
+        qdo = QuData.output(result={
+            'variables': {'q0': 0.0, 'q1': 1.0, 'q2': 1.0},
+            'objective': -2
+        })
+        scipy_result = qdo.to_scipy()
+
+        # シンボリック変数の定義
+        q0, q1, q2 = symbols('q0 q1 q2')
+
+        # 目的関数を定義
+        objective_function = 2 * q0 - q1 - q2
+
+        # シンボリック関数を数値化して評価できる形式に変換
+        f = lambdify([q0, q1, q2], objective_function, 'numpy')
+
+        # 初期解 (すべて0に設定)
+        q = [0.5, 0.5, 0.5]
+
+        # バイナリ変数の範囲を定義 (0 <= x <= 1)
+        bounds = Bounds([0, 0, 0], [1, 1, 1])
+
+        # 制約なしで最適化を行い、結果をバイナリ制約に丸める
+        res = minimize(lambda q: f(q[0], q[1], q[2]), q, method='SLSQP', bounds=bounds)
+
+        # 検証
+        np.testing.assert_array_equal(scipy_result.x, res.x)
+        self.assertEqual(scipy_result.fun, res.fun)
+        self.assertEqual(scipy_result.success, res.success)
 
 if __name__ == '__main__':
     unittest.main()
